8da0d9ecfe1b74a11b8584a2199301c05b69727a
AlkaMotors <37931458+AlkaMotors@users.noreply.github.com>
2025-01-07 14:01:09 -0400

    dshot info request

--- Mcu/f051/Src/serial_telemetry.c
+++ Mcu/f051/Src/serial_telemetry.c
@@ -6,10 +6,10 @@
  */
 
 #include "serial_telemetry.h"
-
+#include "common.h"
 #include "targets.h"
 
-uint8_t aTxBuffer[10];
+uint8_t aTxBuffer[49];
 uint8_t nbDataToTransmit = sizeof(aTxBuffer);
 
 #ifdef USE_PA14_TELEMETRY
@@ -151,10 +151,10 @@ void telem_UART_Init(void)
     LL_DMA_EnableIT_TE(DMA1, LL_DMA_CHANNEL_2);
 }
 
-void send_telem_DMA()
+void send_telem_DMA(uint8_t bytes)
 { // set data length and enable channel to start transfer
     LL_USART_SetTransferDirection(USART1, LL_USART_DIRECTION_TX);
-    LL_DMA_SetDataLength(DMA1, LL_DMA_CHANNEL_2, nbDataToTransmit);
+    LL_DMA_SetDataLength(DMA1, LL_DMA_CHANNEL_2, bytes);
     LL_USART_EnableDMAReq_TX(USART1);
 
     LL_DMA_EnableChannel(DMA1, LL_DMA_CHANNEL_2);
@@ -179,6 +179,13 @@ uint8_t get_crc8(uint8_t* Buf, uint8_t BufLen)
     for (i = 0; i < BufLen; i++)
         crc = update_crc8(Buf[i], crc);
     return (crc);
+}
+
+void makeInfoPacket(){
+   for(int i = 0;i < 48; i++){
+     aTxBuffer[i] = eepromBuffer.buffer[i];
+    }
+    aTxBuffer[48] = get_crc8(aTxBuffer, 48);
 }
 
 void makeTelemPackage(uint8_t temp, uint16_t voltage, uint16_t current,
